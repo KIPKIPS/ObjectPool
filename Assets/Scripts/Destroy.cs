cb50604d79da23e8b07dde5bffaa6640813d8efc
RECNEPS-KIPS <2432719656@qq.com>
2020-03-24 15:55:16 +0800

    实现了将对象放回池中

---
+++ Assets/Scripts/Destroy.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public void Init() {
+        StartCoroutine(ReturnToPool());
+    }
+
+    IEnumerator ReturnToPool() {
+        yield return new WaitForSeconds(2f);
+        //5秒之后调用该方法
+        //将对象放回池中
+        ObjectPoolManager.instance.Return(this.gameObject);
+    }
+}
